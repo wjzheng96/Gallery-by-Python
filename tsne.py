6091e303b2069e0c0df1d9ddf5449e64eedc1866
wjzheng <53817049+wjzheng96@users.noreply.github.com>
2020-10-08 15:33:04 +0800

    Update tsne.py

--- tsne.py
+++ tsne.py
@@ -12,11 +12,12 @@ import matplotlib as mpl
 # mpl.rcParams['font.sans-serif'] = ['FangSong'] # 指定中文字体
 # mpl.rcParams['axes.unicode_minus'] = False # 解决保存图像是负号'-'显示为方块的问题
 
+
 def plot_with_labels(low_dim_embs, labels, filename):   # 绘制词向量图
     assert low_dim_embs.shape[0] >= len(labels), 'More labels than embeddings'
     print('绘制词向量中......')
     plt.figure(figsize=(10, 10))  # in inches
-    mark = ['darkorange', 'greenyellow', 'lightskyblue', 'violet']
+    mark = ['orange', 'yellow', 'lightskyblue', 'violet', 'purple', 'black', 'green']
     color = []
     l = defaultdict(list)
     for i, label in enumerate(labels):
@@ -38,55 +39,64 @@ def plot_with_labels(low_dim_embs, labels, filename):   # 绘制词向量图
     plt.savefig(filename)
     plt.show()
 
-def load_txt_vec(file, threshold=0, dtype='float'):	# 读取txt文件格式的word2vec词向量
+def load_vec(ndarr, threshold=0, dtype='float'):	# 读取txt文件格式的word2vec词向量
     print('读取词向量文件中......')
     #header = file.readline().split(' ')
-    header=[13404, 128]
-    count = int(header[0]) if threshold <= 0 else min(threshold, int(header[0]))
-    dim = int(header[1])
+    # header=[1044, 16]
+    # count = int(header[0]) if threshold <= 0 else min(threshold, int(header[0]))
+    # dim = int(header[1])
+    count = ndarr.shape[0]
+    dim = ndarr.shape[1]
     words = []
     matrix = np.empty((count, dim), dtype=dtype)
     for i in range(count):
-        word=i 
-        line = file.readline().strip().split(' ')
-        words.append(i)   #l ine[0]
-        vec = line
-        matrix[i] = np.array(vec)  # np.fromstring(vec, sep=' ', dtype=dtype)
+        words.append(i)
+        matrix[i] = ndarr[i]  # np.fromstring(vec, sep=' ', dtype=dtype)
     print("#####")
     return (words, matrix)
 
-def read_label(inputFileName):
-    f = open(inputFileName, 'r')
-    lines = f.readlines()
-    f.close()
-    N = len(lines)
-    y = np.zeros(N, dtype=int)
-    for line in lines:
-        l = line.strip('\n\r').split(' ')
-        y[int(l[0])] = int(l[1])
+def read_label(input):
 
-    return y
+    # f = open(inputFileName, 'r')
+    # lines = f.readlines()
+    # f.close()
+    # N = len(lines)
+    # y = np.zeros(N, dtype=int)
+    # for line in lines:
+    #     l = line.strip('\n\r').split(' ')
+    #     y[int(l[0])] = int(l[1])
+    input = np.load(input)
+    N = input.shape[0]
+    y = np.zeros(N,dtype=int)
+    i = 0
+    for i in range(N):
+        y[i] = int(input[i])
+
+    return input
 
 if __name__ == '__main__':
     try:
         # 若要载入txt文件格式的word2vec词向量，则执行下面这两行
         # method = 'dblp_HANE.txt'
         # method = 'DBLP_mile.txt'
-        method = 'dblp_deepwalk.txt'
-        w2v_txt_file = open('./dataset/'+method, 'r')	# txt格式的词向量文件
-        words, vectors = load_txt_vec(w2v_txt_file)	# 载入txt文件格式的word2vec词向量
+        input = 'embeddings_cora_nc.npy'
+        # w2v_txt_file = open(method, 'r')	# txt格式的词向量文件
+        # words, vectors = load_txt_vec(w2v_txt_file)	# 载入txt文件格式的word2vec词向量
+        input = np.load(input)
+        words, vectors = load_vec(input)
         # tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=500, method='exact')
         tsne = TSNE(n_components=2, init='pca')
         # pca = PCA(n_components=2)  # 加载PCA算法，设置降维后主成分数目为2
         # low_dim_embs = pca.fit_transform(vectors)
         #plot_only = 2707	# 限定画点（词向量）的个数，只画词向量文件的前plot_only个词向量
         low_dim_embs = tsne.fit_transform(vectors) # 需要显示的词向量，一般比原有词向量文件中的词向量个数少，不然点太多，显示效果不好
-        np.save('fig_stne/' + method[:-4] + '.npy', low_dim_embs)
-        labels=read_label("./dataset/DBLP/labels.txt")
+        np.save('fig_stne/' + 'cora_tsne.npy', low_dim_embs)
+        labels=read_label("/home/devine/Documents/hgwnn/cora_label.npy")
+
         # low_dim_embs = np.load('node_ax.npy')
         #labels = [words[i] for i in range(plot_only)] # 要显示的点对应的单词列表
         print('paint picture')
-        plot_with_labels(low_dim_embs, labels, 'fig_stne/' + method[:-4] + '.png')
+        plot_with_labels(low_dim_embs, labels, 'fig_stne/' + 'cora_nc_HGWNN_1' + '.png')
 
     except ImportError as ex:
         print('Please install gensim, sklearn, numpy, matplotlib, and scipy to show embeddings.')
